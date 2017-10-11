6bcc889a6607d4fe8b6b20cb4751d0223b0a612f
asimbo <asimbongeni@gmail.com>
2017-10-11 20:40:32 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZSFA0gG0WJE6txkQ-rOEsIuhokGZmtS_mYnKCe1hac0/edit#gid=0';
